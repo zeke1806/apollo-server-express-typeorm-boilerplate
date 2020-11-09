666567680597db46b944fb35886064277ae9021e
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-11-09 22:09:53 +0300

    feat(back): setup graphql server

---
+++ back/src/server.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from "apollo-server-express";
+import compression from "compression";
+import cors from "cors";
+import { createServer } from "http";
+import depthLimit from "graphql-depth-limit";
+import express from "express";
+import schema from "./schema";
+
+const app = express();
+
+const server = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(7)],
+});
+
+app.use("*", cors());
+app.use(compression());
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+const httpServer = createServer(app);
+httpServer.listen({ port: 3000 }, (): void =>
+  console.log(`GraphQL is now running on http://localhost:3000/graphql`)
+);
